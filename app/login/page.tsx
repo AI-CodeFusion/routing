ecadbd0c7fd317c6d36c74efb47ccaf4302c88d7
syeda-codetech12 <sbs86.freelancer@gmail.com>
2024-10-22 01:30:49 +0500

    routing

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div> 
+        <h1 className='text-4xl text-center underline'>Login</h1>
+    </div>
+  )
+}
+
+export default login
---
+++ app/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+        <h1 className='text-4xl text-center underline'>Signup here</h1>
+    </div>
+  )
+}
+
+export default Signup
